e755d278fa7878d96a443ebbcf5b48cd0437e0c7
Martin Lantzsch <martin@linux-doku.de>
2014-08-08 00:13:06 +0200

    renaming

--- tests/BaseExpressionTest.php
+++ tests/BaseExpressionTest.php
@@ -7,21 +7,21 @@ class BaseExpressionTest extends PHPUnit_Framework_TestCase {
     public function testExpressionEquals() {
         $array = [1, 2, 3];
         $queryable = Queryable::create($array);
-        $baseExpression = new ClosureExpressionProvider();
-        $this->assertTrue($queryable->any($baseExpression->equals(1)));
+        $expression = new ClosureExpressionProvider();
+        $this->assertTrue($queryable->any($expression->equals(1)));
     }
 
     public function testExpressionEqualsFail() {
         $array = [1, 2, 3];
         $queryable = Queryable::create($array);
-        $baseExpression = new ClosureExpressionProvider();
-        $this->assertFalse($queryable->any($baseExpression->equals(5)));
+        $expression = new ClosureExpressionProvider();
+        $this->assertFalse($queryable->any($expression->equals(5)));
     }
 
     public function testExpressionGreaterThanOrEqual() {
         $array = [1, 2, 3];
         $queryable = Queryable::create($array);
-        $baseExpression = new ClosureExpressionProvider();
-        $this->assertTrue($queryable->any($baseExpression->greaterThanOrEqual(1)));
+        $expression = new ClosureExpressionProvider();
+        $this->assertTrue($queryable->any($expression->greaterThanOrEqual(1)));
     }
 }

44c2efde44c00a8f373f93bab4a4939647625912
Martin Lantzsch <martin@linux-doku.de>
2014-08-07 22:44:11 +0200

    comments for the query able provider interface

--- src/QueryableProvider.php
+++ src/QueryableProvider.php
@@ -3,12 +3,50 @@ namespace LinuxDoku\Queryable;
 
 interface QueryableProvider
 {
+    /**
+     * How many items are stored in the collection?
+     *
+     * @param null $expression
+     * @return mixed
+     */
     public function count($expression=null);
+
+    /**
+     * Is there any item matching the expression?
+     *
+     * @param null $expression
+     * @return mixed
+     */
     public function any($expression=null);
 
+    /**
+     * Get the first item matching the expression or null if none was found.
+     *
+     * @param null $expression
+     * @return mixed
+     */
     public function firstOrDefault($expression=null);
+
+    /**
+     * Get the last item matching the expression or null if none was found.
+     *
+     * @param null $expression
+     * @return mixed
+     */
     public function lastOrDefault($expression=null);
+
+    /**
+     * Get a new collection which is filtered by the expression.
+     *
+     * @param $expression
+     * @return QueryableProvider
+     */
     public function where($expression);
 
+    /**
+     * Convert the collection to an array.
+     *
+     * @return array
+     */
     public function toArray();
 }
